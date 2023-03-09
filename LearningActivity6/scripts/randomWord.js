032874a7d35c8aa98bfcf180cd54744eb9d7b0e3
Astr0nii <57813530+Astr0nii@users.noreply.github.com>
2023-03-09 11:09:57 +1100

    Learning Activity 5 Upload + 6
    Massive formatting changes for learning activity 6 to make the navbar links more readable. Accessibility options added for tab indexing and access keys with descriptions.

---
+++ LearningActivity6/scripts/randomWord.js
@@ -0,0 +1,6 @@
+var randomWords = ['perfect', 'extravagant', 'amazing', 'spectacular', 'excellent', 'brilliant', 'inspirational',
+                    'striking', 'eye-catching', 'breathtaking', 'glorious', 'flawless', 'complete', 'dazzling',
+                    'clever', 'creative', 'marvellous', 'superb', 'outstanding', 'wonderful'];
+var choosenWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+
+document.getElementById("choosenWord").innerHTML = choosenWord;
