e9407da3580e0e7479d259b0a958c84a80cf858e
Astr0nii <57813530+Astr0nii@users.noreply.github.com>
2023-03-16 15:25:14 +1100

    Website changes

--- LearningActivity6/scripts/randomValues.js
+++ LearningActivity6/scripts/randomValues.js
@@ -1,7 +1,7 @@
 var randomWords = ['perfect', 'extravagant', 'amazing', 'spectacular', 'excellent', 'brilliant', 'inspirational',
                     'striking', 'eye-catching', 'breathtaking', 'glorious', 'flawless', 'complete', 'dazzling',
-                    'clever', 'creative', 'marvellous', 'superb', 'outstanding', 'wonderful'];
-var randomColour = ['#e23500', '#00d3e2', '#e2a600', '#00e24b', '#8f00e2'];
+                    'clever', 'creative', 'marvellous', 'superb', 'outstanding', 'wonderful', 'innovative'];
+var randomColour = ['#e23500', '#00d3e2', '#e2a600', '#00e24b', '#8f00e2', '#cc7f19', '#cc19c3'];
 
 var choosenWord = randomWords[Math.floor(Math.random() * randomWords.length)];
 var choosenColour = randomColour[Math.floor(Math.random() * randomColour.length)];
