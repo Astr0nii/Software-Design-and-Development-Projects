6ba7a80157fff2156c65ac75b7324f483a36248b
Astr0nii <Gam3z4lif3m8@gmail.com>
2023-05-09 22:06:57 +1000

    Username system reworked and now works

--- Assessment/scripts/main.js
+++ Assessment/scripts/main.js
@@ -4,94 +4,64 @@ const randomWords = ['perfect', 'extravagant', 'amazing', 'spectacular', 'excell
                     'elegant', 'precisely crafted', 'fresh', 'forward-thinking', 'remarkable', 'sublime', 'polished',
                     'stunning', 'impressive', 'gorgeous', 'refined'];
 
-const request = indexedDB.open('usernameStorage');
+const introPhrases = ["How do you do", "G'day", "Welcome", "Nice to meet you", "Looking great today", "Hi",
+                     "Hi there", "Today is the day", "Feed your curiosity", "New dawn, new day"];
+
+let request = indexedDB.open('usernameStorage', 1);
+let username;
+
+request.onupgradeneeded = function() {
+    const db = request.result;
+    if (!db.objectStoreNames.contains('users')) {
+        db.createObjectStore("users", { keyPath: 'id' });
+    }
+}
+
+request.onsuccess = function() {
+    db = request.result;
+};
 
 document.addEventListener("DOMContentLoaded", function() {
-    checkUser();
     if (document.title == "Astron") {
-        document.getElementById("replaceName").innerHTML = "";
+        getUser(function(username){
+            document.getElementById("replaceName").innerHTML = username;
+        });
         document.getElementById("choosenWord").innerHTML = randomWords[Math.floor(Math.random() * randomWords.length)];
+        document.getElementById("introduction").innerHTML = introPhrases[Math.floor(Math.random() * introPhrases.length)];
     }
-    else if (document.title == "Welcome to Camden Learning!") {
+    else if (document.title == "Welcome to Astron") {
         document.getElementById("nameButton").addEventListener("click", function() {
             event.preventDefault();
             storeUser(document.getElementById("name").value);
-            checkUser();
+            document.location.href = "../index.html";
         });
     }
 });
 
-function checkUser() {
-    getUsername(username => {
-        const uname = username;
+function storeUser(username) {
+    const db = request.result;
+    const transaction = db.transaction('users', 'readwrite');
+    const usersObjectStore = transaction.objectStore('users');
+    usersObjectStore.put({ id: 1, username });
 
-        console.log("Checking username!")
-        console.log(uname);
-
-        if (uname != "" && document.title != "Astron") {
-            console.log("Username found!");
-            document.location.href = "../index.html";
-        }
-        else if (document.title != "Welcome to Astron") {
-            console.log("Username not found!");
-            document.location.href = "pages/welcome.html";
-        }
-    });
 }
 
 // Function for username storing
-function storeUser(user) {
-
-    // Creates an object store in the onupgradeneeded event handler
-    request.onupgradeneeded = event => {
-        const db = event.target.result;
-        db.createObjectStore('users', { keypath: 'id' });
-    };
-
-    // Error handling when opening the database
-    request.onerror = event => {
-        console.error("Error opening databate:", event.target.error);
-    };
-
-    // Add our username to the database
-    request.onsuccess = event => {
-        const db = event.target.result;
-        const transaction = db.transaction('users', 'readwrite');
-        const usersObjectStore = transaction.objectStore('users');
-        usersObjectStore.add({ id: 1, username: user });
-    }
-
-}
-
-// Function for handling username fetching
-function getUsername(callback) {
-
-    // Check if our DB even exists
-    request.onsuccess = event => {
-        const db = event.target.result;
-        if (db.objectStoreNames.contains('users')) {
-            const db = event.target.result;
+function getUser(callback) {
+        request.onsuccess = function() {
+            const db = request.result;
             const transaction = db.transaction('users', 'readonly');
             const usersObjectStore = transaction.objectStore('users');
             const getRequest = usersObjectStore.get(1);
 
-            
-
-            getRequest.onsuccess = event => {
+            getRequest.onsuccess = function() {
                 if (getRequest.result) {
-                    callback(getRequest.result.username);
+                    username = getRequest.result.username;
+                    callback(username);
                 }
-                else {
-                    switch(document.title) {
-                        case "Astron":
-                            console.log("Username not found!");
-                            document.location.href = "pages/welcome.html";
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            };
+                else if (!getRequest.result && document.title != "Welcome to Astron") {
+                        document.location.href = "pages/welcome.html";
+                    } 
+            }
         }
-    };
 }
